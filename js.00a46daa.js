83b8d162e84b9dff4c397919fc92bbe1ea7db56c
sanyacastor <34678023+sanyacastor@users.noreply.github.com>
2019-10-22 16:31:01 +0300

    Update js.00a46daa.js

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -122,7 +122,7 @@ ymaps.ready(init);
 
 function init() {
   var myMap = new ymaps.Map("map", {
-    center: [55.6849398, 37.8561008],
+    center: [55.6849, 37.8158],
     zoom: 14
   }, {
     searchControlProvider: 'yandex#search'
